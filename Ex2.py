6db2652d777fe7e440fd84a8bd1d04fe11eb9bc1
BurcaStefan <burcaiulianstefan@gmail.com>
2024-10-08 20:11:52 +0300

    delete exercises

--- Ex2.py
+++
@@ -1,8 +0,0 @@
-string=input()
-count=0
-
-for ch in string:
-    if ch in "AEIOUaeiou":
-        count+=1
-
-print("In sirul dat ", string, " vocalele apar de ", count, "ori")
--- Ex3.py
+++
@@ -1,5 +0,0 @@
-string1=input()
-string2=input()
-count=string2.count(string1)
-
-print(string1, " apare de ", count, " ori in ", string2)
